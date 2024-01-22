b6abc69b61ef90d57cba36f0d0ce5168c7598b4a
Caleb <kai_sunny@icloud.com>
2024-01-22 16:15:47 +0800

    first commit

---
+++ src/hw1/hw1.java
@@ -0,0 +1,77 @@
+package hw1;
+
+//  題目1
+//public class hw1 {
+//    public static void main(String[] args) {
+//    	int i =12;
+//    	int j =6;
+//    	System.out.println(12+6);
+//    	System.out.println(12*6); 	
+//    }
+//}
+
+//  題目2
+//public class hw1 {
+//    public static void main(String[] args) {
+//    	int egg = 200;
+//    	int Adozen = 12;
+//    	int result = egg / Adozen;
+//    	System.out.println(result); 	
+//    }
+//}
+
+//  題目3
+public class hw1 {
+    public static void main(String[] args) {
+    	int time = 256559;
+    	int d = 24;
+    	int h = 60;
+    	int m = 60;
+    	int s = 60;
+    	
+    	int resultsecond = time % s;
+//    	int resulthour = resultday % h;
+//    	int resultmintune = resulthour % m;
+//    	int resultsecond =resultmintune % s;
+//    	System.out.println(resultday); 
+//    	System.out.println(resulthour); 
+//    	System.out.println(resultmintune); 
+    	System.out.println(resultsecond); 
+    	
+    }
+}
+
+//  題目4
+//public class hw1 {
+//    public static void main(String[] args) {
+//    	double pi = 3.1415;
+//    	int r = 5;
+//    	double circumference = 2 * r * pi;
+//    	double circleArea = r * r * pi;
+//    	
+//    	System.out.printf("%.5f" ,circumference); 
+//    	System.out.printf("\n%.5f" ,circleArea);     	
+//    }
+//}
+
+//題目5
+//public class hw1 {
+//	public static void main(String[] args) {
+//		int account = 1_500_000;
+//		double interest = 0.02;
+//		double OneYearInterest = 1 + interest;
+//		double TenYearInterest = Math.pow( OneYearInterest,10);
+//		double nowAccount = account * TenYearInterest;
+//	
+//		System.out.printf("%.2f",nowAccount);    	
+//	}
+//}
+
+//題目6
+//public class hw1 {
+//	public static void main(String[] args) {
+//			System.out.println(5 + 5);  // 基本資料型態裡的整數相加
+//			System.out.println(5 + '5');   // ASCll '5' = 53 ，所以5+53= 58
+//			System.out.println(5 + "5");  // 5是基本資料型態的int整數,"5"被定義為字串，兩個不同資料型態無法相加，所以印出55
+//	}
+//}
